4c5577d1e8a6874aebb6f6551c0591447658e410
justinraj89 <rajewskijustin@gmail.com>
2023-01-27 13:48:05 -0800

    rapidAPI calls setup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
+import axios from 'axios';
 //=================================
 
 function Home() {
 
-  const [title, setTitle] = useState('');
   const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState('');
   const [lyrics, setLyrics] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    setSearchResults('');
+    setLyrics('');
+  }
+
+
+  
   return (
     <div className="flex flex-col md:px-12 px-0 relative bg-gray-900 font-serif items-center min-h-screen">
       <h1 className="text-6xl font-bold text-stone-50 mt-10">
@@ -17,11 +30,12 @@ function Home() {
         Music lyrics on demand
       </h2>
 
-      <form className="sm:mx-auto mt-20 justify-center sm:w-full sm:flex">
+      <form className="sm:mx-auto mt-20 justify-center sm:w-full sm:flex" onSubmit={handleSubmit}>
         <input
           type="text"
           className="flex w-full sm:w-1/3 rounded-lg px-5 py-3 text-black font-semibold focus:outline-none focus:ring-2 ring-rose-400 focus:ring-active"
           placeholder="Enter a track or artist name"
+          onChange={handleChange}
         />
         <div className="mt-4 sm:mt-0 sm:ml-3">
           <button
@@ -32,7 +46,7 @@ function Home() {
           </button>
         </div>
       </form>
-      
+
     </div>
   );
 }
